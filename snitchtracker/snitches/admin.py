7b2f4382c73ae3c6e25fbe50f6ca51c3b7c99aad
Rourke750 <Rourke7501@gmail.com>
2018-06-19 15:28:35 -0400

    Fix minor issue with icons not disappearing

--- snitchtracker/snitches/admin.py
+++ snitchtracker/snitches/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Group, Group_Member, Snitch, Snitch_Record, Snitch_Group, Token, WebhookTransaction
-
+from django.contrib.admin.models import LogEntry
 # Register your models here.
 admin.site.register(Group)
 admin.site.register(Group_Member)
@@ -10,3 +10,4 @@ admin.site.register(Snitch_Record)
 admin.site.register(Snitch_Group)
 admin.site.register(WebhookTransaction)
 admin.site.register(Token)
+admin.site.register(LogEntry)
